77da4077f59f2e0861396112ac91c3f3273b9174
sunflowerr3 <109298023+sunflowerr3@users.noreply.github.com>
2022-07-18 19:44:34 +0300

    Add files via upload

---
+++ задача 13.cs
@@ -0,0 +1,10 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("введите число!!");
+string number = Console.ReadLine();
+if (number.Length < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+Console.WriteLine (number [2]);
---
+++ задача 15.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("введите число!!");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Это выходной");
+}
+else if (number > 7 || number < 1)
+{
+    Console.WriteLine ("Неверный ввод");
+}
+else Console.WriteLine ("Будний день");
